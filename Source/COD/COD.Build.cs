e8690cc93c3694ee0793fb2ed4215910bd7686b5
Dove9 <khb532@naver.com>
2025-08-07 18:12:41 +0900

    이사중

--- Source/COD/COD.Build.cs
+++ Source/COD/COD.Build.cs
@@ -9,5 +9,10 @@ public class COD : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		
+		PublicIncludePaths.AddRange(new string[] 
+		{
+			"COD"
+		});
 	}
 }
