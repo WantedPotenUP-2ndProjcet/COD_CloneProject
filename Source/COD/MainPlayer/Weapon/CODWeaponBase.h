413ffeab35bff3d725882fcdb13f777c4daf6932
Dove9 <khb532@naver.com>
2025-08-18 01:55:26 +0900

    From_main

---
+++ Source/COD/MainPlayer/Weapon/CODWeaponBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CODWeaponBase.generated.h"
+
+UCLASS()
+class COD_API ACODWeaponBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACODWeaponBase();
+
+
+
+};
