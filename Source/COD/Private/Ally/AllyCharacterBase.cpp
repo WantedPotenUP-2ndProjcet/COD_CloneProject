2c8a07a9081df129ac2fb3b98612571f3ad5f357
Dove9 <khb532@naver.com>
2025-08-20 13:12:31 +0900

    [FIX] Weapon-asset

--- Source/COD/Private/Ally/AllyCharacterBase.cpp
+++ Source/COD/Private/Ally/AllyCharacterBase.cpp
@@ -18,10 +18,10 @@ AAllyCharacterBase::AAllyCharacterBase()
 
 	if (auto* Move = GetCharacterMovement())
     {
-        Move->MaxWalkSpeed = MoveSpeed;
+        Move->MaxWalkSpeed = 300.f;
         Move->MaxAcceleration = 2048.f;
         Move->BrakingDecelerationWalking = 2048.f;
-        Move->GroundFriction = GroundFriction;
+        Move->GroundFriction = 8.f;
         Move->RotationRate = FRotator(0, 720, 0);
         Move->bOrientRotationToMovement = true;
 
@@ -44,6 +44,7 @@ void AAllyCharacterBase::BeginPlay()
 	Super::BeginPlay();
 
 	HP = MaxHP;
+    GetMesh()->HideBoneByName(TEXT("weapon_r"), EPhysBodyOp::PBO_None);
 
     /* if(WeaponClass)
     {
